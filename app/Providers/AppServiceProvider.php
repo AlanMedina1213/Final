85e3f62879f077ecec1917be03f582326e24e5b9
AlanMedina1213 <135571123+AlanMedina1213@users.noreply.github.com>
2023-06-05 00:20:37 -0600

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if (env('REDIRECT_HTTPS')) {
+       $this->app['request']->server->set('HTTPS', true);
+   }
     }
 
     /**
